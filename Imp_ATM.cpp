5976cf96e04021ab45a433da29fb44de3550a6f0
A_Radaideh <radaideh7484@gmail.com>
2025-05-28 14:25:19 +0300

    Make a Struct for Beneficiary _ first Created!

--- Imp_ATM.cpp
+++ Imp_ATM.cpp
@@ -8,3 +8,15 @@
 
 
 
+struct Beneficiary {
+	std::string User_Name_First_Name;
+	std::string User_Name_Last_Name;
+	char User_Gender;
+	int User_National_ID_Number;
+	int User_Phone_Number;
+	double User_Monthly_Salary_amount;
+	std::string User_Email;
+	int User_ID;
+	double User_Current_Balance;
+
+};
